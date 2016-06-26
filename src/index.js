4ad322f15395a795f557d4e3c66b18b2d37e0b97
Bartosz Dabkowski <BartoszDabkowski@Bartoszs-MacBook-Pro.local>
2016-06-25 22:23:31 -0700

    added custome search.

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ class App extends Component {
             videos: [],
             selectedVideo: null 
         };
-        
+
         this.videoSearch('surfboards');
     }
 
@@ -31,7 +31,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
